20ddee6be5a9dc839465473e9c49df6bb50fc4b6
Julie Kuehl <julie@juliekuehl.com>
2014-09-13 20:25:55 -0500

    Login screen styling
    styled link and button colors

--- wp-content/plugins/jumpstart-login/custom-login.php
+++ wp-content/plugins/jumpstart-login/custom-login.php
@@ -3,7 +3,7 @@
 Plugin Name: Jump Start Login
 Plugin URI: https://github.com/JulieKuehl
 Description: This plugin customizes the WordPress login screen.
-Version: 10.0
+Version: 1.0
 Author: Julie Kuehl
 Author URI: http://juliekuehl.com
 License: GPLv2
@@ -14,6 +14,25 @@ function jumpstart_login_logo() { ?>
 <style type="text/css">
     .login #login h1 a {
         background-image: url( <?php echo plugins_url( 'media/login-logo.png' , __FILE__ ); ?> );
+        background-size: 300px auto;
+        height: 100px;
+        width: 300px;
+    }
+    .login #nav a,
+    .login #backtoblog a {
+        color: #878787 !important;
+    }
+    .login #nav a:hover,
+    .login #backtoblog a:hover {
+        color: #f7931e !important;
+    }
+    .login .button-primary {
+        background: #f7931e;
+        border: 0;
+    }
+    .login .button-primary:hover {
+        background: #c3681e;
+        border: 0;
     }
 </style>
 <?php }
